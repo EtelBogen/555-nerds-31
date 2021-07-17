c03abc4d66a955fde61f87fa42f793eb854744d3
EtelBogen <etel.bogen@gmail.com>
2021-07-17 16:29:11 +0300

    Правки по замечаниям проверяющего наставника-2
    1. исправлено переполнение
    2. добавлена анимация для формы (появление и ошибка)
    3. минифицированы css и js, подключены

--- js/script.js
+++ js/script.js
@@ -2,25 +2,56 @@
 const writeUs = document.querySelector(".write-us");
 const contactsPopup = document.querySelector(".modal-contacts");
 const contactsClose = contactsPopup.querySelector(".modal-close");
+const modalForm = contactsPopup.querySelector(".modal-form");
 const contactsName = contactsPopup.querySelector(".modal-contacts-name");
+const contactsEmail = contactsPopup.querySelector(".modal-contacts-email");
 
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 writeUs.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactsPopup.classList.add("modal-show");
-  contactsName.focus();
+  if (storage) {
+    contactsEmail.value = storage;
+  } else {
+    contactsName.focus();
+  }
 });
 
+//Ошибка
 contactsClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactsPopup.classList.remove("modal-show");
+  contactsPopup.classList.remove("modal-error");
 });
 
+//Валидация формы
+
+modalForm.addEventListener("submit", function (evt) {
+  if (!contactsName.value || !contactsEmail.value) {
+    evt.preventDefault();
+    contactsPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("email", contactsEmail.value);
+    }
+  }
+});
+
+//Кнопка Esc
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (contactsPopup.classList.contains("modal-show")) {
       evt.preventDefault();
       contactsPopup.classList.remove("modal-show");
+      contactsPopup.classList.remove("modal-error");
     }
   }
 });
